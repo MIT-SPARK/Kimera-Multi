369effa770a301f86da57fcd1f9b15f74223c676
RonghaiHe <hrhkjys@qq.com>
2024-04-06 11:53:54 +0800

    Add missing dependencies

--- install/dependencies.sh
+++ install/dependencies.sh
@@ -13,7 +13,11 @@ apt-get install -y \
       gfortran
 apt-get install -y libtbb-dev
 apt-get install -y libgflags-dev libgoogle-glog-dev
-apt-get install -y python-wstool python-catkin-tools ros-noetic-cmake-modules protobuf-compiler autoconf
+apt-get install -y python-wstool python-catkin-tools ros-${ROS_DISTRO}-cmake-modules protobuf-compiler autoconf
 # apt install -y ros-melodic-mesh-tools
-apt install -y ros-noetic-ompl
+apt-get install -y libgtk2.0-dev opencl-clhpp-headers
+apt install -y ros-${ROS_DISTRO}-pcl-ros ros-${ROS_DISTRO}-tf-conversions ros-${ROS_DISTRO}-camera-info-manager ros-${ROS_DISTRO}-interactive-markers ros-${ROS_DISTRO}-rviz 
+apt install -y ros-${ROS_DISTRO}-ompl
 apt-get install -y build-essential cmake-gui libsuitesparse-dev
+
+pip install vcstool empy==3.3.4 catkin_pkg numpy
